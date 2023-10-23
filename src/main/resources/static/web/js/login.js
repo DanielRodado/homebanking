104c05824cf76275774d2198333a4bc312bdf87d
Daniel Rodado <d4nielrodado@gmail.com>
2023-10-22 22:23:08 -0500

    Se modificaron los etilos de 'login'. Ahora 'login' y 'register' están juntos en: 'login.html' y 'login.js'

--- src/main/resources/static/web/js/login.js
+++ src/main/resources/static/web/js/login.js
@@ -5,22 +5,56 @@ createApp({
         return {
             inputEmail: "",
             inputPassword: "",
+
+            inputNameRegister: "",
+            inputLastNameRegister: "",
+            inputEmailRegister: "",
+            inputPasswordRegister: "",
         };
     },
 
-    created() {
-        
-    },
+    created() {},
 
     methods: {
         login() {
             axios
-                .post("/api/login", `email=${this.inputEmail}&password=${this.inputPassword}`)
+                .post(
+                    "/api/login",
+                    `email=${this.inputEmail}&password=${this.inputPassword}`
+                )
                 .then((response) => {
-                        console.log(response);
-                        location.pathname = "/web/pages/accounts.html";
-                    })
+                    console.log(response);
+                    location.pathname = "/web/pages/accounts.html";
+                })
                 .catch((error) => console.log(error));
         },
+        register() {
+            axios
+                .post(
+                    "/api/clients",
+                    `firstName=${this.inputNameRegister}&lastName=${this.inputLastNameRegister}&email=${this.inputEmailRegister}&password=${this.inputPasswordRegister}`
+                )
+                .then(() => {
+                    console.log("User registered");
+
+                    axios
+                        .post(
+                            "/api/login",
+                            `email=${this.inputEmailRegister}&password=${this.inputPasswordRegister}`
+                        )
+                        .then((response) => {
+                            console.log(response);
+                            location.pathname = "/web/pages/accounts.html";
+                        })
+                        .catch((error) => console.log(error));
+                })
+                .catch((error) => {
+                    console.error("Error registering user:", error);
+                });
+        },
+
+        logout() {
+            axios.post('/api/logout').then(response => console.log('signed out!!!'))
+        }
     },
 }).mount("#app");
