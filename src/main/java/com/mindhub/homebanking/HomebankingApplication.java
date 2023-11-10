ee081e76ba26d5c07689831d43e79429d92444ce
Daniel Rodado <d4nielrodado@gmail.com>
2023-11-09 19:41:19 -0500

    Se quitó una inyección de dependencia que impedia el funcionaiento de los test

--- src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -132,6 +132,6 @@ public class HomebankingApplication {
 			accountRepository.save(accountClientAdmin);*/
 		};
 	}
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+	/*@Autowired
+	private PasswordEncoder passwordEncoder;*/
 }
