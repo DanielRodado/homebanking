516b460d87ce6db4eddf21852039b37f4e2e98a4
Daniel Rodado <d4nielrodado@gmail.com>
2023-11-17 16:18:27 -0500

    Se agregó un nuevo test para el formateo de la fecha

--- src/test/java/com/mindhub/homebanking/utilsTests/LoanUtilTest.java
+++ src/test/java/com/mindhub/homebanking/utilsTests/LoanUtilTest.java
@@ -3,6 +3,10 @@ package com.mindhub.homebanking.utilsTests;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDateTime;
+
+import static com.mindhub.homebanking.utils.LoanUtil.formattedLocalDateTime;
 import static com.mindhub.homebanking.utils.LoanUtil.formatterStringStartUpperEndLower;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -10,6 +14,11 @@ import static org.hamcrest.Matchers.*;
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class LoanUtilTest {
+
+    @Test
+    public void localDateTimeTest() {
+        assertThat(formattedLocalDateTime(LocalDateTime.now()), notNullValue());
+    }
 
     @Test
     public void formatterStringTest() {
