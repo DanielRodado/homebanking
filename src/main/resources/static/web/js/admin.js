523e3219a642b9b3faf6bf5343770ff8a8b5586b
Daniel Rodado <d4nielrodado@gmail.com>
2023-11-18 13:19:53 -0500

    Se creó una vista para los clientes con rol 'ADMIN'; un pequeño panel de control para crear nuevos préstamos, crear más administradores y convertir un cliente en administrador

--- src/main/resources/static/web/js/admin.js
+++ src/main/resources/static/web/js/admin.js
@@ -10,7 +10,16 @@ createApp({
             loading: true,
             paymentAdd: "",
             payments: [],
-            isAdmin: null
+            isAdmin: null,
+
+            adminFirstName: "",
+            adminLastName: "",
+            adminEmail: "",
+            adminPass: "",
+
+            emailClientToAdmin: "",
+
+            valueInputPanelAdmin: "Create Loan"
         };
     },
 
@@ -40,10 +49,17 @@ createApp({
                     Swal.fire({
                         title: "Loan created!",
                         text: `The loan '${this.typeOfLoan}' has been successfully created.`,
+                        customClass: {
+                            popup: 'text-center'
+                        },
                         icon: "success",
                         color: "#fff",
                         background: "#1c2754",
                     });
+                    this.typeOfLoan = "";
+                    this.maxAmount = null;
+                    this.interestRate = null;
+                    this.payments = [];
                 })
                 .catch((error) => this.messageError(error.response.data));
         },
@@ -72,24 +88,7 @@ createApp({
             });
         },
         createLoan() {
-            Swal.fire({
-                title: "Are you sure?",
-                text: "Do you want to create a new type of loan?",
-                customClass: {
-                    popup: 'text-center'
-                },
-                icon: "warning",
-                showCancelButton: true,
-                color: "#fff",
-                background: "#1c2754",
-                confirmButtonColor: "#17acc9",
-                cancelButtonColor: "#d33",
-                confirmButtonText: "Yes, create!",
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    this.createNewLoan();
-                }
-            });
+            this.alertSW("Do you want to create a new type of loan?", "Yes, create!", this.createNewLoan)
         },
         addPayments() {
             if (this.paymentAdd === "") {
@@ -165,6 +164,79 @@ createApp({
                     }
                 });
             }
+        },
+        createNewAdmin() {
+            const obj = {
+                firstName: this.adminFirstName,
+                lastName: this.adminLastName,
+                email: this.adminEmail,
+                password: this.adminPass,
+                createAdmin: true
+            }
+            axios
+                .post("/api/clients", obj)
+                .then(() => {
+                    Swal.fire({
+                        title: "Admin created!",
+                        text: `The admin '${this.adminFirstName} ${this.adminLastName}' has been successfully created.`,
+                        customClass: {
+                            popup: 'text-center'
+                        },
+                        icon: "success",
+                        color: "#fff",
+                        background: "#1c2754",
+                    });
+                    this.adminFirstName = "";
+                    this.adminLastName = "";
+                    this.adminEmail = "";
+                    this.adminPass = "";
+                })
+                .catch((error) => this.messageError(error.response.data));
+        },
+        createAdmin() {
+            this.alertSW("Do you want to create a new admin?", "Yes, create new admin!", this.createNewAdmin)
+        },
+        clientToAdmin() {
+            axios
+                .patch("/api/clients/admin", `clientEmail=${this.emailClientToAdmin}`)
+                .then(() => {
+                    Swal.fire({
+                        title: "Done!",
+                        text: `The client with email ${this.emailClientToAdmin} has become admin.`,
+                        customClass: {
+                            popup: 'text-center'
+                        },
+                        icon: "success",
+                        color: "#fff",
+                        background: "#1c2754",
+                    });
+                    this.emailClientToAdmin = "";
+                })
+                .catch((error) => this.messageError(error.response.data));
+        },
+        convertClientToAdmin() {
+            this.alertSW("Do you want to convert this client to admin?", "Yes, convert!", this.clientToAdmin)
+        },
+        alertSW(message, buttonText, requestMethod) {
+            Swal.fire({
+                title: "Are you sure?",
+                text: message,
+                customClass: {
+                    popup: 'text-center'
+                },
+                icon: "warning",
+                showCancelButton: true,
+                color: "#fff",
+                background: "#1c2754",
+                confirmButtonColor: "#17acc9",
+                cancelButtonColor: "#d33",
+                confirmButtonText: buttonText,
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    requestMethod();
+                }
+            });
         }
+
     }
 }).mount("#app");
