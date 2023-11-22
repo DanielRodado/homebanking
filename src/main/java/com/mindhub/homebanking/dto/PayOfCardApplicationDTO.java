d7858adb5177614c6781993a23779077a0bd69a5
DanielRodado <d4nielrodado@gmail.com>
2023-11-22 17:45:41 -0500

    Se agregó un duevo DTO para recibir información para el pago externo con una tarjeta

---
+++ src/main/java/com/mindhub/homebanking/dto/PayOfCardApplicationDTO.java
@@ -0,0 +1,27 @@
+package com.mindhub.homebanking.dto;
+
+public class PayOfCardApplicationDTO {
+
+    private String numberCard, cvvCard, description;
+
+    private Double amount;
+
+    public PayOfCardApplicationDTO() {
+    }
+
+    public String getNumberCard() {
+        return numberCard;
+    }
+
+    public String getCvvCard() {
+        return cvvCard;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+}
