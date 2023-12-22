a60db7c5fe7b3fcca59bd42aac0b87c0c7604f79
DanielRodado <d4nielrodado@gmail.com>
2023-12-22 13:23:59 -0500

    Propiedades agregadas a las entidades

--- src/main/java/com/mindhub/homebanking/models/Account.java
+++ src/main/java/com/mindhub/homebanking/models/Account.java
@@ -1,4 +1,88 @@
 package com.mindhub.homebanking.models;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "native")
+    private Long id;
+
+    private String number;
+
+    private LocalDate creationDate;
+
+    private Double balance;
+
+    private AccountType accountType;
+
+    private Boolean isDeleted = false;
+
+    @ManyToOne()
+    private Client client;
+
+    public Account() {
+    }
+
+    public Account(String number, LocalDate creationDate, Double balance, AccountType accountType) {
+        this.number = number;
+        this.creationDate = creationDate;
+        this.balance = balance;
+        this.accountType = accountType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public AccountType getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(AccountType accountType) {
+        this.accountType = accountType;
+    }
+
+    public Boolean getDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        isDeleted = deleted;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
 }
--- src/main/java/com/mindhub/homebanking/models/Client.java
+++ src/main/java/com/mindhub/homebanking/models/Client.java
@@ -1,4 +1,78 @@
 package com.mindhub.homebanking.models;
 
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
 public class Client {
+
+    // Properties
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "native")
+    private Long id;
+
+    private String name, lastName, email, password;
+
+    @OneToMany(mappedBy = "client", fetch = FetchType.EAGER)
+    private Set<Account> accounts = new HashSet<>();
+
+    // Constructors
+
+    public Client() {
+    }
+
+    public Client(String name, String lastName, String email, String password) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Accessory methods
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // Methods
+
+    public void addAccount(Account account) {
+        this.accounts.add(account);
+        account.setClient(this);
+    }
 }
