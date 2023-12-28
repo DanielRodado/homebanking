c589b57ef064803c66d52f48e9eee26d825b1dfd
DanielRodado <d4nielrodado@gmail.com>
2023-12-28 18:07:46 -0500

    Configuración de seguridad

---
+++ src/main/java/com/mindhub/homebanking/configurations/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.mindhub.homebanking.configurations;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(ant ->
+                ant.requestMatchers(HttpMethod.POST, "/api/clients", "/api/cards/pay").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/clients/current").permitAll()
+                .requestMatchers(HttpMethod.GET, "/index.html", "/web/pages/login.html", "/web/pages/register.html",
+                                "/web/css/**", "/web/js/**", "/web/assets/**").permitAll()
+                        .requestMatchers("/rest/**").hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/clients", "/api/clients/{id}",
+                                "/api/loans/create", "/api/accounts", "/api/accounts/**").hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/h2-console/**", "/web/pages/manager.html",
+                                "/web/pages/admin-loan.html").hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.PATCH, "/api/clients/admin").hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/web/pages/**", "/api/clients/current/**", "/api/loans").hasAuthority("CLIENT")
+                        .requestMatchers(HttpMethod.POST,"/api/clients/current/**", "/api/loans", "/api/loans/pay").hasAuthority("CLIENT")
+                        .requestMatchers(HttpMethod.PATCH, "/api/clients/current/accounts/delete",
+                                "/api/clients/current/cards/delete").hasAuthority("CLIENT")
+                        .anyRequest().denyAll())
+
+                .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(hFrameOpt -> hFrameOpt.disable()));
+
+        http.formLogin(formLogin ->
+                formLogin
+                        .loginPage("/web/login.html")
+                        .loginProcessingUrl("/api/login")
+                        .usernameParameter("email")
+                        .passwordParameter("password")
+                        .successHandler((request, response, authentication) -> clearAuthenticationAttributes(request))
+                        .failureHandler((request, response, exception) -> response.sendError(403)))
+                        .logout(logout -> logout
+                                .logoutUrl("/api/logout")
+                                .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler())
+                                .deleteCookies("JSESSIONID"))
+
+                .rememberMe(Customizer.withDefaults()); /* Mantiene la sesión en el servidor desde que te logueas
+                                                  hasta que de deslogueas */
+
+        return http.build();
+    }
+
+    private void clearAuthenticationAttributes(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+        }
+    }
+
+}
--- src/main/java/com/mindhub/homebanking/controllers/ClientController.java
+++ src/main/java/com/mindhub/homebanking/controllers/ClientController.java
@@ -1,11 +1,14 @@
 package com.mindhub.homebanking.controllers;
 
 import com.mindhub.homebanking.DTO.ClientDTO;
+import com.mindhub.homebanking.services.AccountService;
 import com.mindhub.homebanking.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 
@@ -14,10 +17,42 @@ import java.util.Set;
 public class ClientController {
 
     @Autowired
-    private ClientService clientServices;
+    private ClientService clientService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AccountService accountService;
 
     @GetMapping("/clients")
     public Set<ClientDTO> getAllClientsDTO() {
-        return clientServices.getAllClientsDTO();
+        return clientService.getAllClientsDTO();
+    }
+
+    @GetMapping("/client/{id}")
+    public ClientDTO getClientById(@PathVariable Long id) {
+        return clientService.getClientDTOById(id);
+    }
+
+    @PatchMapping("clients/admin")
+    public ResponseEntity<String> modifyClientToAdmin(@RequestParam String clientEmail) {
+
+        if (!clientService.existsClientByEmail(clientEmail)) {
+            return new ResponseEntity<>("The client you want to modify to admin does not exist.", HttpStatus.FORBIDDEN);
+        }
+
+        if (clientService.existsClientByEmailAndIsAdmin(clientEmail, true)) {
+            return new ResponseEntity<>("The client to be modified to admin is already an admin.", HttpStatus.FORBIDDEN);
+        }
+
+        clientService.modifyClientToAdminByEmail(clientEmail);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/clients/current")
+    public ClientDTO getClientCurrent(Authentication currentClient) {
+        return new ClientDTO(clientService.getClientByEmail(currentClient.getName()));
     }
 }
